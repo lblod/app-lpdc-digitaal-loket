9bbcc986c8c614cd4a508d536a783c35b50d9577
Riad Abdallah <riad.abdallah@hotmail.com>
2023-06-28 14:28:24 +0300

    Enable deltas for lpdc-management and lpdc-publish

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -1,9 +1,9 @@
-// import lpdcManagement from './lpdc-management';
-// import lpdcPublish from './lpdc-publish';
+import lpdcManagement from './lpdc-management';
+import lpdcPublish from './lpdc-publish';
 import resource from './resource';
 
 export default [
-//   ...lpdcManagement,
-//   ...lpdcPublish,
+  ...lpdcManagement,
+  ...lpdcPublish,
   ...resource
 ];
