9c2309047ea5afdf8720974589efa2f9d3a42b86
gauquiebart <bart.gauquie@gmail.com>
2023-08-31 09:55:18 +0200

    LPDC-551: mimic failing test

--- tests/test-api/bestuurseenheid/bestuurseenheden-resource.spec.ts
+++ tests/test-api/bestuurseenheid/bestuurseenheden-resource.spec.ts
@@ -8,7 +8,7 @@ test(`Can get a bestuurseenheid`, async ({request}) => {
     expect(await pepingen.json()).toMatchObject({
         data: {
             attributes: {
-                naam: "Pepingen",
+                naam: "Pepingendd",
                 uri: "http://data.lblod.info/id/bestuurseenheden/73840d393bd94828f0903e8357c7f328d4bf4b8fbd63adbfa443e784f056a589"
             },
             id: "73840d393bd94828f0903e8357c7f328d4bf4b8fbd63adbfa443e784f056a589",
