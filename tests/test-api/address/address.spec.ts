056d9beb7cb536ed04775ff73182ab557b20b0eb
joren <joren.kennes@kunlabora.be>
2023-09-27 11:02:32 +0200

    LPDC-677: add api test for fetch streets endpoint

--- tests/test-api/address/address.spec.ts
+++ tests/test-api/address/address.spec.ts
@@ -51,3 +51,52 @@ test(`find municipality for postcode '3001'`, async ({request}) => {
     expect(actual).toEqual(['Leuven']);
 });
 
+test(`find street for municipality 'Aarschot' and search 'kerk'`, async ({request}) => {
+
+    const params = {municipality: 'Aarschot', search: 'kerk'};
+    const response = await request.get(`${dispatcherUrl}/lpdc-management/address/streets/`, {params});
+
+    expect(response.ok(), `Error ${response.status()}`).toBeTruthy();
+    const actual = await response.json();
+    expect(actual).toEqual(['Kerkstraat', 'Kerkvoetweg', 'Kerkweg']);
+});
+
+test(`find street for municipality 'Aarschot' and search 'ke'`, async ({request}) => {
+
+    const params = {municipality: 'Aarschot', search: 'ke'};
+    const response = await request.get(`${dispatcherUrl}/lpdc-management/address/streets/`, {params});
+
+    expect(response.ok(), `Error ${response.status()}`).toBeTruthy();
+    const actual = await response.json();
+    expect(actual).toEqual([]);
+});
+
+test(`find street for municipality 'Aarschot' and search 'unknown street'`, async ({request}) => {
+
+    const params = {municipality: 'Aarschot', search: 'unknown street'};
+    const response = await request.get(`${dispatcherUrl}/lpdc-management/address/streets/`, {params});
+
+    expect(response.ok(), `Error ${response.status()}`).toBeTruthy();
+    const actual = await response.json();
+    expect(actual).toEqual([]);
+});
+
+test(`find street for no municipality`, async ({request}) => {
+
+    const params = {search: 'unknown street'};
+    const response = await request.get(`${dispatcherUrl}/lpdc-management/address/streets/`, {params});
+
+    expect(response.ok(), `Error ${response.status()}`).toBeTruthy();
+    const actual = await response.json();
+    expect(actual).toEqual([]);
+});
+
+test(`find street for unknown municipality`, async ({request}) => {
+
+    const params = {municipality: 'unknown municipality', search: 'Kerkstraat'};
+    const response = await request.get(`${dispatcherUrl}/lpdc-management/address/streets/`, {params});
+
+    expect(response.ok(), `Error ${response.status()}`).toBeTruthy();
+    const actual = await response.json();
+    expect(actual).toEqual([]);
+});
