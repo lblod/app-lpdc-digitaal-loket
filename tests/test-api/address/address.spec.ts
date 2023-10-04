0b8c8c1fa085cf50534e31ba3007aa174d36ece5
joren <joren.kennes@kunlabora.be>
2023-10-04 18:06:20 +0200

    LPDC-677: expand response body for address validate endpoint in tests

--- tests/test-api/address/address.spec.ts
+++ tests/test-api/address/address.spec.ts
@@ -108,37 +108,15 @@ test(`validate address for correct address`, async ({request}) => {
 
     expect(response.ok(), `Error ${response.status()}`).toBeTruthy();
     const actual = await response.json();
-    expect(actual).toEqual({ volledigAdres: "Ten Drossaarde 1, 3200 Aarschot" });
-});
-
-test(`validate address for invalid address`, async ({request}) => {
-
-    const params = {municipality: 'Aarschot', street: 'Ten Drossaarde', houseNumber: '1', busNumber: '1'};
-    const response = await request.get(`${dispatcherUrl}/lpdc-management/address/validate`, {params});
-
-    expect(response.ok(), `Error ${response.status()}`).toBeTruthy();
-    const actual = await response.json();
-    expect(actual).toEqual({ volledigAdres: undefined });
-});
-
-test(`validate address for invalid address where letter of houseNumber in put in busNumber field`, async ({request}) => {
-
-    const params = {municipality: 'Torhout', street: 'Aartrijkestraat', houseNumber: '11', busNumber: 'B'};
-    const response = await request.get(`${dispatcherUrl}/lpdc-management/address/validate`, {params});
-
-    expect(response.ok(), `Error ${response.status()}`).toBeTruthy();
-    const actual = await response.json();
-    expect(actual).toEqual({ volledigAdres: undefined });
-});
-
-test(`validate address for invalid address where houseNumber contains '/'`, async ({request}) => {
-
-    const params = {municipality: 'Torhout', street: 'Aartrijkestraat', houseNumber: '11/B'};
-    const response = await request.get(`${dispatcherUrl}/lpdc-management/address/validate`, {params});
-
-    expect(response.ok(), `Error ${response.status()}`).toBeTruthy();
-    const actual = await response.json();
-    expect(actual).toEqual({ volledigAdres: undefined });
+    expect(actual).toEqual({
+        gemeente: 'Aarschot',
+        postcode: '3200',
+        straat: 'Ten Drossaarde',
+        huisnummer: '1',
+        busnummer: undefined,
+        volledigAdres: "Ten Drossaarde 1, 3200 Aarschot",
+        adressenRegisterId: 'https://data.vlaanderen.be/id/adres/2648372'
+    });
 });
 
 test(`validate address for address where letter of number is correctly put in houseNumber field`, async ({request}) => {
@@ -148,8 +126,83 @@ test(`validate address for address where letter of number is correctly put in ho
 
     expect(response.ok(), `Error ${response.status()}`).toBeTruthy();
     const actual = await response.json();
-    expect(actual).toEqual({ volledigAdres: 'Aartrijkestraat 11B, 8820 Torhout' });
+    expect(actual).toEqual({
+        gemeente: 'Torhout',
+        postcode: '8820',
+        straat: 'Aartrijkestraat',
+        huisnummer: '11B',
+        busnummer: undefined,
+        volledigAdres: 'Aartrijkestraat 11B, 8820 Torhout',
+        adressenRegisterId: "https://data.vlaanderen.be/id/adres/3654524"
+    });
 });
+
+test(`validate address for address with bus number`, async ({request}) => {
+
+    const params = {municipality: 'Harelbeke', street: 'Generaal Deprezstraat', houseNumber: '2', busNumber: '50'};
+    const response = await request.get(`${dispatcherUrl}/lpdc-management/address/validate`, {params});
+
+    expect(response.ok(), `Error ${response.status()}`).toBeTruthy();
+    const actual = await response.json();
+    expect(actual).toEqual({
+        gemeente: 'Harelbeke',
+        postcode: '8530',
+        straat: 'Generaal Deprezstraat',
+        huisnummer: '2',
+        busnummer: '0050',
+        volledigAdres: 'Generaal Deprezstraat 2 bus 0050, 8530 Harelbeke',
+        adressenRegisterId: "https://data.vlaanderen.be/id/adres/5516749"
+    });
+});
+
+test(`validate address for address range`, async ({request}) => {
+
+    const params = {municipality: 'Kruibeke', street: 'O.L. Vrouwplein', houseNumber: '18-20'};
+    const response = await request.get(`${dispatcherUrl}/lpdc-management/address/validate`, {params});
+
+    expect(response.ok(), `Error ${response.status()}`).toBeTruthy();
+    const actual = await response.json();
+    expect(actual).toEqual({
+        gemeente: 'Kruibeke',
+        postcode: '9150',
+        straat: 'O.L.Vrouwplein',
+        huisnummer: '18',
+        busnummer: undefined,
+        volledigAdres: 'O.L.Vrouwplein 18, 9150 Kruibeke',
+        adressenRegisterId: "https://data.vlaanderen.be/id/adres/548604",
+    });
+});
+
+test(`validate address for invalid address`, async ({request}) => {
+
+    const params = {municipality: 'Aarschot', street: 'Ten Drossaarde', houseNumber: '1', busNumber: '1'};
+    const response = await request.get(`${dispatcherUrl}/lpdc-management/address/validate`, {params});
+
+    expect(response.ok(), `Error ${response.status()}`).toBeTruthy();
+    const actual = await response.json();
+    expect(actual).toEqual({});
+});
+
+test(`validate address for invalid address where letter of houseNumber in put in busNumber field`, async ({request}) => {
+
+    const params = {municipality: 'Torhout', street: 'Aartrijkestraat', houseNumber: '11', busNumber: 'B'};
+    const response = await request.get(`${dispatcherUrl}/lpdc-management/address/validate`, {params});
+
+    expect(response.ok(), `Error ${response.status()}`).toBeTruthy();
+    const actual = await response.json();
+    expect(actual).toEqual({});
+});
+
+test(`validate address for invalid address where houseNumber contains '/'`, async ({request}) => {
+
+    const params = {municipality: 'Torhout', street: 'Aartrijkestraat', houseNumber: '11/B'};
+    const response = await request.get(`${dispatcherUrl}/lpdc-management/address/validate`, {params});
+
+    expect(response.ok(), `Error ${response.status()}`).toBeTruthy();
+    const actual = await response.json();
+    expect(actual).toEqual({});
+});
+
 
 test(`validate address throws error when municipality is missing`, async ({request}) => {
 
