eaa988138340d5e9a8e245a09d63affaee2f14fd
joren <joren.kennes@kunlabora.be>
2024-01-31 16:20:13 +0100

    LPDC-917: add api tests for reopen instance

---
+++ tests/test-api/instantie/reopen-instance.spec.ts
@@ -0,0 +1,41 @@
+import {expect, test} from "@playwright/test";
+import {dispatcherUrl} from "../test-helpers/test-options";
+import {loginAsPepingen, pepingenId} from "../test-helpers/login";
+import {PublicServiceTestBuilder, PublicServiceType} from "../test-helpers/public-service.test-builder";
+import {InstanceStatus} from "../test-helpers/codelists";
+import {fetchType} from "../test-helpers/sparql";
+import {Predicates} from "../test-helpers/triple-array";
+
+
+test.describe('Reopen instance', () =>  {
+
+
+    test('Reopen sent instance', async ({request}) => {
+        const loginResponse = await loginAsPepingen(request);
+        const modified = new Date();
+        const instance = await PublicServiceTestBuilder.aPublicService()
+            .withInstanceStatus(InstanceStatus.verstuurd)
+            .withModified(modified)
+            .buildAndPersist(request, pepingenId)
+
+        const response = await request.put(`${dispatcherUrl}/lpdc-management/public-services/${encodeURIComponent(instance.getId().getValue())}/reopen`, {params: {cookie: loginResponse.cookie}});
+        expect(response.ok(), await response.text()).toBeTruthy();
+
+        const updatedInstance = await fetchType(request, instance.getSubject().getValue(), PublicServiceType);
+        expect(updatedInstance.findTriple(Predicates.instanceStatus).getObjectValue()).toBe('http://lblod.data.gift/concepts/instance-status/ontwerp');
+        expect(updatedInstance.findTriple(Predicates.modified).getObjectValue()).not.toEqual(modified.toISOString());
+    });
+
+    test('Reopen draft instance should throw error', async ({request}) => {
+        const loginResponse = await loginAsPepingen(request);
+        const modified = new Date();
+        const instance = await PublicServiceTestBuilder.aPublicService()
+            .withInstanceStatus(InstanceStatus.ontwerp)
+            .withModified(modified)
+            .buildAndPersist(request, pepingenId)
+
+        const response = await request.put(`${dispatcherUrl}/lpdc-management/public-services/${encodeURIComponent(instance.getId().getValue())}/reopen`, {params: {cookie: loginResponse.cookie}});
+        expect(response.ok(), await response.text()).toBeFalsy();
+    });
+
+});
