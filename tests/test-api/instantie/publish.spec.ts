cd0bff912dab6fb0a681f2e2f3e6c7a59e403a64
joren <joren.kennes@kunlabora.be>
2024-02-01 13:07:38 +0100

    LPDC-917: add test for api call publish instance

--- tests/test-api/instantie/publish.spec.ts
+++ tests/test-api/instantie/publish.spec.ts
@@ -1,32 +1,16 @@
 import {expect, test} from "@playwright/test";
-import {dispatcherUrl} from "../test-helpers/test-options";
 import {loginAsPepingen, pepingenId} from "../test-helpers/login";
 import {PublicServiceTestBuilder, PublicServiceType} from "../test-helpers/public-service.test-builder";
 import {InstanceStatus} from "../test-helpers/codelists";
+import {dispatcherUrl} from "../test-helpers/test-options";
 import {fetchType} from "../test-helpers/sparql";
 import {Predicates} from "../test-helpers/triple-array";
 
 
-test.describe('Reopen instance', () =>  {
+test.describe('publish instance', () => {
 
 
-    test('Reopen sent instance', async ({request}) => {
-        const loginResponse = await loginAsPepingen(request);
-        const modified = new Date();
-        const instance = await PublicServiceTestBuilder.aPublicService()
-            .withInstanceStatus(InstanceStatus.verstuurd)
-            .withModified(modified)
-            .buildAndPersist(request, pepingenId)
-
-        const response = await request.put(`${dispatcherUrl}/lpdc-management/public-services/${encodeURIComponent(instance.getId().getValue())}/reopen`, {params: {cookie: loginResponse.cookie}});
-        expect(response.ok(), await response.text()).toBeTruthy();
-
-        const updatedInstance = await fetchType(request, instance.getSubject().getValue(), PublicServiceType);
-        expect(updatedInstance.findTriple(Predicates.instanceStatus).getObjectValue()).toBe('http://lblod.data.gift/concepts/instance-status/ontwerp');
-        expect(updatedInstance.findTriple(Predicates.modified).getObjectValue()).not.toEqual(modified.toISOString());
-    });
-
-    test('Reopen draft instance should throw error', async ({request}) => {
+    test('publish instance', async ({request}) => {
         const loginResponse = await loginAsPepingen(request);
         const modified = new Date();
         const instance = await PublicServiceTestBuilder.aPublicService()
@@ -34,11 +18,27 @@ test.describe('Reopen instance', () =>  {
             .withModified(modified)
             .buildAndPersist(request, pepingenId)
 
-        const response = await request.put(`${dispatcherUrl}/lpdc-management/public-services/${encodeURIComponent(instance.getId().getValue())}/reopen`, {params: {cookie: loginResponse.cookie}});
+        const response = await request.put(`${dispatcherUrl}/lpdc-management/public-services/${encodeURIComponent(instance.getId().getValue())}/publish`, {params: {cookie: loginResponse.cookie}});
+        expect(response.ok(), await response.text()).toBeTruthy();
+
+        const updatedInstance = await fetchType(request, instance.getSubject().getValue(), PublicServiceType);
+        expect(updatedInstance.findTriple(Predicates.instanceStatus).getObjectValue()).toBe('http://lblod.data.gift/concepts/instance-status/verstuurd');
+        expect(updatedInstance.findTriple(Predicates.modified).getObjectValue()).not.toEqual(modified.toISOString());
+    });
+
+    test('publish already published instance', async ({request}) => {
+        const loginResponse = await loginAsPepingen(request);
+        const modified = new Date();
+        const instance = await PublicServiceTestBuilder.aPublicService()
+            .withInstanceStatus(InstanceStatus.verstuurd)
+            .withModified(modified)
+            .buildAndPersist(request, pepingenId)
+
+        const response = await request.put(`${dispatcherUrl}/lpdc-management/public-services/${encodeURIComponent(instance.getId().getValue())}/publish`, {params: {cookie: loginResponse.cookie}});
         expect(response.ok(), await response.text()).toBeFalsy();
 
         const updatedInstance = await fetchType(request, instance.getSubject().getValue(), PublicServiceType);
-        expect(updatedInstance.findTriple(Predicates.instanceStatus).getObjectValue()).toBe('http://lblod.data.gift/concepts/instance-status/ontwerp');
+        expect(updatedInstance.findTriple(Predicates.instanceStatus).getObjectValue()).toBe('http://lblod.data.gift/concepts/instance-status/verstuurd');
         expect(updatedInstance.findTriple(Predicates.modified).getObjectValue()).toEqual(modified.toISOString());
     });
 
