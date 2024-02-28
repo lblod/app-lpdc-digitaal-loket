650c6860c1e7a3da58f55d72c3b1e6747437bb6c
thuysbr <brent.thuys@kunlabora.be>
2024-02-28 14:22:00 +0100

    LPDC-1026: add migrations for legal resources

---
+++ migration-scripts/instance-legal-resources/instance-legal-resources.ts
@@ -0,0 +1,97 @@
+//@ts-ignore
+import SparqlClient from "sparql-client-2";
+//@ts-ignore
+import fs from "fs";
+import { v4 as uuid } from "uuid";
+
+
+
+async function main() {
+    const instances =await getAllInstancesWithLegalResources();
+    console.log(`"${instances.length}" instanties te controleren`);
+    const quads=[]
+
+    let legalResources =0
+    for (const instance of instances){
+        instance.legalResources = await getAllLegalResourcesForInstance(instance);
+        legalResources +=instance.legalResources.length
+        const instanceQuads :string[] = legalResourcesFromInstanceToQuad(instance);
+        quads.push(...instanceQuads)
+    }
+    console.log(`"${legalResources}" legalResources te controleren`);
+
+    fs.writeFileSync(`./migration-results/legalResources.ttl`, quads.join('\n'));
+}
+function legalResourcesFromInstanceToQuad(instance: Instance): string[] {
+    const quads =[]
+    let order =1
+    for(const lrUrl of instance.legalResources) {
+        const uniqueId = uuid()
+        const legalResourceId = `http://data.lblod.info/id/legal-resource/${uniqueId}`
+
+        const hasLegalResource = `<${instance.instanceUri}> <http://data.europa.eu/m8g/hasLegalResource> <${legalResourceId}> <${instance.graph}> .`
+        const legalResourceType = `<${legalResourceId}> a <http://data.europa.eu/eli/ontology#LegalResource> <${instance.graph}> .`
+        const legalResourceUuid = `<${legalResourceId}> <http://mu.semte.ch/vocabularies/core/uuid> "${uniqueId}" <${instance.graph}> .`
+        const legalResourceUrl = `<${legalResourceId}> <http://schema.org/url> """${lrUrl}""" <${instance.graph}> .`
+        const legalResourceOrder = `<${legalResourceId}> <http://www.w3.org/ns/shacl#order> "${order}"^^<http://www.w3.org/2001/XMLSchema#integer> <${instance.graph}> .`
+
+        quads.push(hasLegalResource)
+        quads.push(legalResourceType)
+        quads.push(legalResourceUuid)
+        quads.push(legalResourceUrl)
+        quads.push(legalResourceOrder)
+        order++;
+    }
+    return quads
+
+}
+
+
+async function executeQuery(query: any) {
+    const sparqlClient = new SparqlClient(process.env.SPARQL_URL);
+    const response = await sparqlClient.query(query).executeRaw();
+    return JSON.parse(response.body)?.results?.bindings;
+}
+
+async function getAllInstancesWithLegalResources():Promise<Instance[]> {
+    const query = `
+        SELECT DISTINCT ?instance ?graph WHERE {
+            GRAPH ?graph{
+                ?instance a  <http://purl.org/vocab/cpsv#PublicService>.
+                ?instance <http://data.europa.eu/m8g/hasLegalResource> ?legalResource.
+            }    
+        }        
+        `;
+
+    const response = await executeQuery(query);
+    return response.map((binding: any) => (
+        {
+        instanceUri: binding.instance.value,
+        legalResources:[],
+        graph:binding.graph.value
+    }));
+}
+
+async function getAllLegalResourcesForInstance(instance: Instance):Promise<string[]> {
+    const query = `
+        SELECT ?legalResource WHERE {
+            GRAPH <${instance.graph}>{
+                ?instance a  <http://purl.org/vocab/cpsv#PublicService>.
+                ?instance <http://data.europa.eu/m8g/hasLegalResource> ?legalResource.
+                FILTER(?instance = <${instance.instanceUri}> )
+            }  
+        }        
+        `;
+
+    const response =await executeQuery(query);
+    return response.map((binding: any) => binding.legalResource.value);
+}
+
+
+type Instance = {
+    instanceUri: string,
+    legalResources: string[],
+    graph: string,
+};
+
+main();
