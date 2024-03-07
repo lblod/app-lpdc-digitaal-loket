76083a1870c37cd02f0a1001f44235aec0592442
joren <joren.kennes@kunlabora.be>
2024-03-07 11:00:01 +0100

    LPDC-768: add test for catch all route ldpc-management

---
+++ tests/test-api/error.spec.ts
@@ -0,0 +1,15 @@
+import {expect, test} from "@playwright/test";
+import {dispatcherUrl} from "./test-helpers/test-options";
+import {loginAsPepingen} from "./test-helpers/login";
+
+
+test('invalid route in lpdc-management returns not found error response', async ({request}) => {
+    await loginAsPepingen(request);
+    const response = await request.get(`${dispatcherUrl}/lpdc-management/public-services/non-exisiting-url`);
+
+    expect(response.ok()).toBe(false);
+    expect(response.status()).toEqual(404);
+    expect(await response.json()).toEqual(expect.objectContaining({
+        _message: 'Niet gevonden'
+    }));
+});
