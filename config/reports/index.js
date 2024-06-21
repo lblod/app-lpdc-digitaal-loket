acb9a13c94dd658dcfc9b3b11af28ed20a4b202e
gauquiebart <bart.gauquie@gmail.com>
2024-06-21 07:42:03 +0200

    LPDC-1002: configure instanceSnapshotProcessingReport in reporting

--- config/reports/index.js
+++ config/reports/index.js
@@ -2,10 +2,12 @@ import lpdcBestuurseenheidReport from './lpdcBestuurseenheidReport';
 import lpdcConceptsReport from './lpdcConceptsReport';
 import instancesStuckinPublishingForLPDCReport from './instancesStuckinPublishingForLPDCReport';
 import formalInformalChoiceLPDCReport from "./formalInformalChoiceLPDCReport";
+import instanceSnapshotProcessingReport from "./instanceSnapshotProcessingReport";
 
 export default [
-  lpdcBestuurseenheidReport,
-  lpdcConceptsReport,
-  instancesStuckinPublishingForLPDCReport,
-  formalInformalChoiceLPDCReport
+    lpdcBestuurseenheidReport,
+    lpdcConceptsReport,
+    instancesStuckinPublishingForLPDCReport,
+    formalInformalChoiceLPDCReport,
+    instanceSnapshotProcessingReport,
 ];
