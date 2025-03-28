41b29bbed9432b070efbe98b4ee45134dc9abcdf
onodrim <alexander.van.den.berghe@redpencil.io>
2025-03-28 16:29:13 +0100

    feat: add reports to list organizations without authority levels
    These reports allow to keep track of new organizations that are added to the
    IPDCOrganisaties code list without a corresponding competente and/or executing
    authority level.

---
+++ config/reports/organizationsWithoutCompetentAuthorityLevel.js
@@ -0,0 +1,39 @@
+import { generateReportFromQueryResult } from "./util/report-helpers";
+import { batchedQuery } from "../helpers.js";
+
+export default {
+  cronPattern: "0 23 6 * * *",
+  name: "organizationsWithoutCompetentAuthorityLevel",
+  execute: async () => {
+    const reportInfo = {
+      title: "List of organisations without a competent authority level",
+      description:
+        "An overview of all organizations in the IPDCOrganisaties code list that are not linked to a competent authority level",
+      filePrefix: "organizationsWithoutCompetentAuthorityLevel",
+    };
+
+    const queryString = `
+      PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+      PREFIX lpdc: <http://data.lblod.info/vocabularies/lpdc/>
+      PREFIX besluit: <http://data.vlaanderen.be/ns/besluit#>
+
+      SELECT DISTINCT ?name ?classification ?uri
+      WHERE {
+        GRAPH <http://mu.semte.ch/graphs/public> {
+          ?uri skos:inScheme <https://productencatalogus.data.vlaanderen.be/id/conceptscheme/IPDCOrganisaties>.
+          FILTER NOT EXISTS { ?uri lpdc:organizationCompetencyLevel ?level. }
+          ?uri skos:prefLabel ?name.
+          OPTIONAL {
+            ?uri besluit:classificatie/skos:prefLabel ?classification.
+          }
+        }
+      } ORDER BY ?prefLabel ?classification
+      `;
+
+    // NOTE (28/03/2025): Use `batchedQuery` as at the time of writing the
+    // levels were not yet added the data and the query just returns all
+    // organisations.
+    const queryResponse = await batchedQuery(queryString);
+    await generateReportFromQueryResult(queryResponse, reportInfo);
+  },
+};
